d8f2f5b9f39c296c15d3ce0131f6f23ba5794cc7
jhanslarico11 <jhanslarico11@gmail.com>
2023-10-15 23:38:42 -0500

    INTEDAD

--- ms-config-server/src/test/java/com/example/msconfigserver/MsConfigServerApplicationTests.java
+++ ms-config-server/src/test/java/com/example/msconfigserver/MsConfigServerApplicationTests.java
@@ -1,14 +1,13 @@
-package com.example.mscarrito;
+package com.example.msconfigserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+class MsConfigServerApplicationTests {
 
-class MsCarritoApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
